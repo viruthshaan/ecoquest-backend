d72319513b16e1267dc7e50f0feb035ed22d8f91
unknown <viruthshaan30@gmail.com>
2024-03-14 14:29:38 +0530

    error checked after crossorigin updated

--- src/main/java/com/ecoquest/game/controller/McqController.java
+++ src/main/java/com/ecoquest/game/controller/McqController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
